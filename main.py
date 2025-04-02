182b2d97c600160449bf59e74ce2128b60f9a327
Nastya <astarikova316@gmail.com>
2025-04-02 19:40:09 +0300

    fhu

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 
+import os 
+os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
@@ -10,28 +12,37 @@ class LikeApp(App):
         self.mainBox = BoxLayout(orientation="vertical")
         self.lbl_title = Label(text='rate this picture', size_hint=[1, 0.1], font_size=32)
         self.img = Image(source="photo_2024-10-08_16-20-36.jpg")
-        btn_layout = BoxLayout(size_hint=[1, 0.2], padding=50, spacing=50)
+        btn_layout = BoxLayout(size_hint=[1, 0.], padding=50, spacing=50)
 
-        btn_like = Button(text="Like", font_size=24, size_hint=[0.45,0.7], padding=50, background_color=[0, 1, 0,1])
-        btn_dislike = Button(text="disike", font_size=24, size_hint=[0.45,0.7], padding=50, background_color=[1,0,0,1])
+        self.stars = []
+
+        for i in range(5):
+            btn = Button(text = f"{i+1}",
+                color = [0,0,0,0],
+                background_normal = "C:/Users/user/Desktop/kivy/те що треба/beginner_git/star0.png",
+                background_down = "C:/Users/user/Desktop/kivy/те що треба/beginner_git/star0.png",
+                on_press = self.rate)
+            self.stars.append(btn)
+            btn_layout.add_widget(btn)
 
         self.mainBox.add_widget(self.lbl_title)
         self.mainBox.add_widget(self.img)
 
-        btn_layout.add_widget(btn_like)
-        btn_layout.add_widget(btn_dislike)
-
         self.mainBox.add_widget(btn_layout)
 
-        btn_like.bind(on_press=self.like)
-        btn_dislike.bind(on_press=self.dislike)
+        
+
 
         return self.mainBox
 
-    def like(self, btn):
-        print('liked')
-    
-    def dislike(self, btn):
-        print('disliked')
-
+    def rate(self,btn):
+        index = int(btn.text)-1
+        for i in range(len(self.stars)):
+            if i <= index:
+                self.stars[i].background_normal = "C:/user/Desktop/kivy/те що треба/beginner_gitstar1.png"
+                self.stars[i].background_down = "C:/Users/user/Desktop/kivy/те що треба/beginner_git/star1.png"
+            else:
+                self.stars[i].background_normal = "C:/Users/user/Desktop/kivy/те що треба/beginner_git/star0.png"
+                self.stars[i].background_down = "C:/Users/user/Desktop/kivy/те що треба/beginner_git/star0.png"
+    print(rate)
 LikeApp().run()
